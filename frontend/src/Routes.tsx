d1a73497b51fa89ea4dcd36113f8d990dd10b1cf
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-01 16:49:53 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,27 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+function Routes() {
+
+    return(
+
+        <BrowserRouter>
+
+            <Switch>
+
+                <Route path="/" exact>
+                    <Home/>
+                </Route>
+
+                <Route path="/dashBord">
+                    <Dashboard/>
+                </Route>
+
+            </Switch>
+
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
