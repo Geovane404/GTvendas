22c953dc4a917c77093a2484f182a0a46ba47b06
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-03 16:38:03 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,6 @@
+import { type } from "os";
+import { Seller } from "./seller";
+
 export type SaleSum = {
 
     sallerName: string;
@@ -11,4 +14,27 @@ export type SaleSuccess = {
     visited: number;
     deals: number;
 
+}
+
+export type Sale = {
+
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
 }
